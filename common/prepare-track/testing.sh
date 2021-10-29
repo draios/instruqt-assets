f61b56637a70362f4df5331725466569646b61a6
pablopez <pablo.lopezzaldivar@sysdig.com>
2021-10-29 14:48:33 +0200

    [falco101/exceptions] app dep fix
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

---
+++ common/prepare-track/testing.sh
@@ -0,0 +1,19 @@
+DATE=$(date +%s)
+TIMETO=$(( $DATE ))
+TIMETO=${TIMETO%?}
+TIMETO+=0
+TIMEFROM=$(( $TIMETO - 100 ))
+TIMETO=$(( $TIMETO + 100 ))
+#echo "DATE: " $DATE "TIMETO: " $TIMETO "TIMEFROM: " $TIMEFROM
+TIMETO+=000000
+TIMEFROM+=000000
+cp /Users/pablopez/GIT/instruqt-assets/common/prepare-track/new_data.original.json /Users/pablopez/GIT/instruqt-assets/common/prepare-track/data.json
+sed -i -e 's/"_TO"/'"$TIMETO"'/g' /Users/pablopez/GIT/instruqt-assets/common/prepare-track/data.json
+sed -i -e 's/"_FROM"/'"$TIMEFROM"'/g' /Users/pablopez/GIT/instruqt-assets/common/prepare-track/data.json
+
+curl -H "Content-Type: application/json"\
+    -H 'Authorization: Bearer b2138da9-3046-4b7b-8998-c053e8dff01f' \
+    --request POST \
+    --data @./data.json \
+    "https://app.sysdigcloud.com//api/data/entity/metadata" #\
+# | jq
