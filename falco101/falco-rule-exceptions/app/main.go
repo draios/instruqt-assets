60e12debe4740a887fa057727d5873020c89299c
pablopez <pablo.lopezzaldivar@sysdig.com>
2021-10-29 16:13:06 +0200

    [falco101/exceptions] write under etc
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

--- falco101/falco-rule-exceptions/app/main.go
+++ falco101/falco-rule-exceptions/app/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-    // "fmt"
-    // "html"
-    // "log"
-    // "net/http"
 	"io/ioutil"
 	"os"
     "fmt"
@@ -13,24 +9,13 @@ import (
 
 func main() {
 
-    // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    //     fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    // })
-
-    // http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-    //     fmt.Fprintf(w, "Hi")
-    // })
-
-    // log.Fatal(http.ListenAndServe(":8081", nil))
+    // https://github.com/falcosecurity/event-generator/blob/b8ed1a9420ff46926b5bf936832f1af1f799cd5c/events/syscall/write_below_etc.go
     for {
-		const filename = "/etc/binary"
-        fmt.Println("Writing to %s", filename)
-		defer os.Remove(filename)
-		return ioutil.WriteFile(filename, nil, os.FileMode(0755))	
         time.Sleep(time.Second)
+        const filename = "/etc/binary"
+        fmt.Println("Writing to %s", filename)
+        defer os.Remove(filename)
+        ioutil.WriteFile(filename, nil, os.FileMode(0755))
     }
-
 }
 
-
-// https://github.com/falcosecurity/event-generator/blob/b8ed1a9420ff46926b5bf936832f1af1f799cd5c/events/syscall/write_below_etc.go
--- falco101/falco-rule-exceptions/run.sh
+++ falco101/falco-rule-exceptions/run.sh
@@ -1,12 +1,15 @@
 #!/bin bash
+# git clone https://github.com/draios/instruqt-assets.git
+cp -r /root/instruqt-assets/falco101/falco-rule-exceptions/* /root/
+rm -rf instruqt-assets/
 
-apt install podman
-apt install buildah
+apt install podman buildah -y
 
-
-buildah bud -t my_app .
-podman run -dt -p 8083:8080/tcp localhost/my_app
-
+buildah bud -t my_app /root./app
+# podman run -dt -p 8083:8080/tcp localhost/my_app
+podman save -o image.tar  localhost/my_app
+k3s ctr images import image.tar
 
 kubectl create -f app/deployment.yaml
+
 kubectl expose deployment my_app --type=NodePort --name=my-app-svc --target-port=8081 
