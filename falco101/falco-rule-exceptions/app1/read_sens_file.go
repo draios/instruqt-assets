db128ab1d5135cc0bf50ffbd919aa9326c876985
pablopez <pablo.lopezzaldivar@sysdig.com>
2021-11-10 14:58:05 +0100

    [falco101/falco-rule-exceptions] update apps
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

--- falco101/falco-rule-exceptions/app1/read_sens_file.go
+++ falco101/falco-rule-exceptions/app1/read_sens_file.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
+    "os"
     "fmt"
     "time"
+    // "math/rand"
 )
 
 // https://github.com/falcosecurity/event-generator/blob/b8ed1a9420ff46926b5bf936832f1af1f799cd5c/events/syscall/read_sensitive_file_untrusted.go
@@ -14,7 +14,9 @@ func main() {
     for {
         const filename = "/etc/shadow"
         fmt.Println("%d - Sleeping 1 second and reading sensitive file: %s", counter, filename)
-        time.Sleep(time.Second)
+        time.Sleep(15 * time.Second)
+        // treinta := rand.Int31n(30)
+        // time.Sleep(time.Duration(treinta) * time.Second)
         file, _ := os.Open(filename)
         defer file.Close()
         counter = counter + 1
