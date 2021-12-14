8fdc4b57e9eba1cb4f43541471c7dc2d143f182f
Luca Avalle <luca.avalle@sysdig.com>
2021-12-14 18:15:00 +0000

    Fix error with wrong collector
    Code needs a space after the quotes at the end of the line.
    https://sysdigcloud.slack.com/archives/GPW3T4T7V/p1639504464006900

--- common/prepare-track/init.sh
+++ common/prepare-track/init.sh
@@ -326,7 +326,7 @@ function test_agent () {
     if [ "$connected" = true ]
     then
         FOUND_COLLECTOR=`kubectl logs -l app.kubernetes.io/instance=sysdig-agent -n sysdig-agent --tail=-1 2> /dev/null | grep "collector:" | head -n1 | rev | cut -d' ' -f 1 | rev`
-        if [ "${FOUND_COLLECTOR}" == "${AGENT_COLLECTOR}"]
+        if [ "${FOUND_COLLECTOR}" == "${AGENT_COLLECTOR}" ]
         then
             echo "  Sysdig Agent successfully installed."
             touch $WORK_DIR/user_data_AGENT_OK
