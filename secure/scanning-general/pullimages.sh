034f19d07099b8b52c0aef9af2289e2c56ff6218
Pablo Musa <pablo.musa@sysdig.com>
2021-12-24 15:12:46 +0100

    [scanning-general] Update track scripts to use podman instead of docker

---
+++ secure/scanning-general/pullimages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Pulling the following containers
+- learnsysdig/node:10.8.0
+- learnsysdig/nginx:1.10.0
+- learnsysdig/nginx:1.15.0
+- learnsysdig/nginx:1.16.0
+- learnsysdig/nginx:1.17.0
+- learnsysdig/dummy-vuln-app
+"
+
+podamn pull learnsysdig/node:10.8.0 &
+podamn pull learnsysdig/nginx:1.10.0 &
+podamn pull learnsysdig/nginx:1.15.0 &
+podamn pull learnsysdig/nginx:1.16.0 &
+podamn pull learnsysdig/nginx:1.17.0 &
+podamn pull learnsysdig/dummy-vuln-app &
+wait
+podamn images | grep learnsysdig
--- secure/scanning-general/pushtodockerhub.sh
+++ secure/scanning-general/pushtodockerhub.sh
@@ -2,10 +2,9 @@
 
 source .reponame
 
-docker push $REPONAME/node:10.8.0
-docker push $REPONAME/nginx:1.10.0
-docker push $REPONAME/nginx:1.15.0
-docker push $REPONAME/nginx:1.16.0
-docker push $REPONAME/nginx:1.17.0
-docker push $REPONAME/dummy-vuln-app
-# docker push $REPONAME/python:2.7.17
+podman push $REPONAME/node:10.8.0
+podman push $REPONAME/nginx:1.10.0
+podman push $REPONAME/nginx:1.15.0
+podman push $REPONAME/nginx:1.16.0
+podman push $REPONAME/nginx:1.17.0
+podman push $REPONAME/dummy-vuln-app
