5f51b6e548be030eaa753f96eb1870a18beb6a03
NathStevo97 <nathan.stephenson@codification.io>
2022-06-07 19:39:09 +0100

    javaapp load gen script

---
+++ monitor/troubleshooting-java-networks/generateload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+count=0
+EXTERNALIP=`kubectl get services -n java-application|grep java-app-svc | awk '{print $4}'`
+while true
+do
+  echo count: $count
+  ab -t 60 -n 10000000 -c 500 -s 1 http://$EXTERNALIP:8080/
+  ((count=count+1))
+done
