95f61f7e5d4b4491054fc436f9b8e6d98a99d873
Pablo Musa <pmusa@users.noreply.github.com>
2022-06-27 10:36:49 +0200

    [prometheus-exporter-postgresql] clean up scripts (#68)

--- monitor/prometheus-exporter-postgresql/manifests/requests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl exec -it postgres-7c875964dd-vfr4t -c postgres -- pgbench -i -d postgresdb -U postgresadmin 
-
-watch -n 11 kubectl exec -it postgres-7c875964dd-vfr4t -c postgres -- pgbench -i -d postgresdb -U postgresadmin
-
-watch -n 43 kubectl exec -it postgres-7c875964dd-vfr4t -c postgres -- pgbench -i -d postgresdb -U postgresadmin 
