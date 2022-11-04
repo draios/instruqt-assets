6369f248452711dbbbceabe4166876848a8f21fd
pablopez <pablo.lopezzaldivar@sysdig.com>
2022-11-04 10:25:12 +0100

    redirect tf logs for aws
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

--- common/prepare-track/cloud/aws/cloud-connector-aws.tf
+++ common/prepare-track/cloud/aws/cloud-connector-aws.tf
@@ -32,4 +32,7 @@ provider "aws" {
 
 module "secure-for-cloud_example_single-account" {
   source = "sysdiglabs/secure-for-cloud/aws//examples/single-account"
+
+  # deploy_beta_image_scanning_ecr=true
+
 }
