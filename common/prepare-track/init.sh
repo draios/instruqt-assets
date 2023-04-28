6190f0c7e723877ef54a9b22601530f18fba184c
pablopez <pablo.lopezzaldivar@sysdig.com>
2023-04-28 15:48:38 +0200

    provision user integrated with existing setup
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

--- common/prepare-track/init.sh
+++ common/prepare-track/init.sh
@@ -30,6 +30,7 @@ AGENT_COLLECTOR=''
 NIA_ENDPOINT=''
 SKIP_CLEANUP=false
 
+USE_USER_PROVISIONER=false
 USE_AGENT=false
 USE_MONITOR_API=false
 USE_SECURE_API=false
@@ -61,6 +62,69 @@ function panic_msg () {
     echo "You can ask for help using Intercom or get in touch with us at team-training@sysdig.com"
     exit 1
 }
+
+##
+# User provisioner, creates a user in a general training Sysdig account
+# so the user does not have to use its own.
+##
+function user_provisioner () {
+    # parent account data
+    ACCOUNT_PROVISIONER_SECURE_API_TOKEN=[REDACTED]
+    ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY=[REDACTED]
+    ACCOUNT_PROVISIONER_REGION=1
+    OLD_STRING="http {"
+
+    # new user creds
+    SPA_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
+    echo ${SPA_PASS}
+    echo "${SPA_PASS}" > $WORK_DIR/ACCOUNT_PROVISIONED_PASS
+    agent variable set SPA_PASS ${SPA_PASS}
+    SPA_USER=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 3 ; echo '')
+    SPA_USER=sysdig_pa_$(date +%Y%m%d)_${SPA_USER}@sysdig.com
+    echo ${SPA_USER}
+    echo "${SPA_USER}" > $WORK_DIR/ACCOUNT_PROVISIONED_USER
+    agent variable set SPA_USER ${SPA_USER}
+
+    # always disable onboarding, just in case someone enables it
+    curl -k -X POST \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer ${SECURE_API_TOKEN}" \
+    --data-binary '{ "onboardingEnabled": false }' \
+    ${SECURE_API_URL}/api/secure/onboarding/v2/feature/status \
+    | jq > /opt/sysdig/account.json
+
+    # create user in parent account
+    curl -s -k -X POST \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer ${SECURE_API_TOKEN}" \
+    --data-binary '{
+    "username": "'${SPA_USER}'",
+    "password": "'${SPA_PASS}'",
+    "firstName": "Training", 
+    "lastName": "Student", 
+    "systemRole": "ROLE_USER" 
+    }' \
+    ${SECURE_API_URL}/api/user/provisioning/ \
+    | jq > $WORK_DIR/account.json
+    # todo proceed only if successful
+
+    # we reuse the existing test data, add here the new credentials
+    # also, we guarantee that for runs where both conditions (is_test_run AND user_provisioner) are meet
+    # we test it with the user provisioned data
+    TEST_AGENT_ACCESS_KEY=$(echo ${ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY} | base64)
+    TEST_MONITOR_API=$(cat /opt/sysdig/account.json | jq --raw-output .token.key | base64)
+    TEST_SECURE_API=$(cat /opt/sysdig/account.json | jq --raw-output .token.key | base64)
+    TEST_REGION=2
+
+    # adaptar
+    # echo ${SPA_USER}
+    # echo "${SPA_USER}" > $WORK_DIR/ACCOUNT_PROVISIONED_USER
+    # agent variable set SPA_USER ${SPA_USER}
+
+    # set flag user provisioned
+    touch $WORK_DIR/user_provisioned_COMPLETED
+}
+
 
 ##
 # Define nginx.conf values to enable redirect
@@ -208,10 +272,10 @@ function select_region () {
     echo "   5) AP Australia (Sydney) - au1"
     echo
 
-    if [[ ${INSTRUQT_USER_ID} == "testuser-"* ]]; 
+    if [ ${INSTRUQT_USER_ID} == "testuser-"* ] || [ "${USE_USER_PROVISIONER}" == true ];
     then
         REGION_N=${TEST_REGION}
-        echo "   Instruqt test Sysdig SaaS region will be used."
+        echo "   Instruqt test or provided Sysdig SaaS region will be used."
     else
         read -p "   Select Region (type number): "  REGION_N; 
     fi
@@ -279,7 +343,7 @@ function configure_API () {
     do
         attempt=$(( $attempt + 1 ))
 
-        if [[ ${INSTRUQT_USER_ID} == "testuser-"* ]]; 
+        if [ ${INSTRUQT_USER_ID} == "testuser-"* ] || [ "${USE_USER_PROVISIONER}" == true ];
         then
             if [[ ${PRODUCT} == "MONITOR" ]];
             then
@@ -438,7 +502,7 @@ function deploy_agent () {
     echo "Configuring Sysdig Agent"
     echo -e "  Visit ${F_BOLD}${F_CYAN}$MONITOR_URL/#/settings/agentInstallation${F_CLEAR} to retrieve your Sysdig Agent Key."
 
-    if [[ ${INSTRUQT_USER_ID} == "testuser-"* ]]; 
+    if [ ${INSTRUQT_USER_ID} == "testuser-"* ] || [ "${USE_USER_PROVISIONER}" == true ];
     then
         AGENT_ACCESS_KEY=$(echo -n ${TEST_AGENT_ACCESS_KEY} | base64 --decode)
     else
