def66fbe963245bde9138818cbabb407d38d6a88
pablopez <pablo.lopezzaldivar@sysdig.com>
2023-05-01 06:09:04 +0200

    split user provision fix delete
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

--- common/prepare-track/account-provisioner-user-delete.sh
+++ common/prepare-track/account-provisioner-user-delete.sh
@@ -6,22 +6,19 @@ set -xe
 # User deprovisioner
 ##
 function user_deprovisioner () {
+
     # parent account data
     ACCOUNT_PROVISIONER_SECURE_API_TOKEN=[REDACTED]
-    echo "${ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY}" > $WORK_DIR/ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY
-    ACCOUNT_PROVISIONER_REGION=1
-    SECURE_API_URL=
-
+    ACCOUNT_PROVISIONER_SECURE_API_URL=https://secure.sysdig.com
     SPA_USER=$(cat $WORK_DIR/ACCOUNT_PROVISIONED_USER)
 
     # delete user in parent account
     curl -s -k -X DELETE \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer ${ACCOUNT_PROVISIONER_SECURE_API_TOKEN}" \
-    ${SECURE_API_URL}/api/users:"'${SPA_USER}'" \
+    ${ACCOUNT_PROVISIONER_SECURE_API_URL}/api/users/${SPA_USER} \
     | jq > $WORK_DIR/delete-user.json
 
 }
-
 
 user_deprovisioner
