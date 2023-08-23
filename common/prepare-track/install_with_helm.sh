775c669ae0e838e7db998cbc553a457fddecf83e
pablopez <pablo.lopezzaldivar@sysdig.com>
2023-08-23 15:57:26 +0200

    fix: set AC endpoint
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

--- common/prepare-track/install_with_helm.sh
+++ common/prepare-track/install_with_helm.sh
@@ -25,6 +25,7 @@ HELM_OPTS="--set admissionController.enabled=true \
     --set admissionController.verifySSL=false \
 	--set admissionController.features.k8sAuditDetections=true \
 	--set admissionController.scanner.enabled=false \
+    --set admissionController.sysdig.apiEndpoint=${COLLECTOR} \
 	--set admissionController.sysdig.secureAPIToken=${SECURE_API_TOKEN} ${HELM_OPTS}"
 
 if [ "$USE_NODE_ANALYZER" = true ]
