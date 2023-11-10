3babec718a4458fb0f6fd0bc6b86033b79a22743
pablopez <pablo.lopezzaldivar@sysdig.com>
2023-11-10 16:08:47 +0100

    Update account-provisioner-user-create.sh

--- common/prepare-track/account-provisioner-user-create.sh
+++ common/prepare-track/account-provisioner-user-create.sh
@@ -126,6 +126,9 @@ curl -s -k -X POST \
 ]' ${ACCOUNT_PROVISIONER_SECURE_API_URL}/api/secure/onboarding/v2/userProfile/questionnaire \
 | jq > /dev/null
 
+# TODO: get monitor operations team ID
+MONITOR_OPS_TEAM_ID=10018845
+
 # get monitor operations team info
 curl -s -k -X GET \
 -H "Content-Type: application/json" \
