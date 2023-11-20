c20f48159841e4f678b9c55590b921baa9fa8e80
Jujuyeh <jujuyeh@protonmail.com>
2023-11-20 17:13:57 +0100

    Add new challenges manifests

--- monitor/troubleshooting-with-advisor/deploy-manifests.sh
+++ monitor/troubleshooting-with-advisor/deploy-manifests.sh
@@ -5,5 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 kubectl create ns application
 kubectl create ns stress-test
 kubectl create ns stress-test-cpu
+kubectl label node worker1 dedicated=test
+kubectl create ns distributed-db
 
 kubectl apply -f ${DIR}/manifests/
