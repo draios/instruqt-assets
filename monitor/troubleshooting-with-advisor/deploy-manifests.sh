e745103e0313eba71ba617260bc17c0bfbd38544
Mateo Burillo <mateo.burillo@sysdig.com>
2023-11-23 14:46:05 +0100

    Replace CPU stress example

--- monitor/troubleshooting-with-advisor/deploy-manifests.sh
+++ monitor/troubleshooting-with-advisor/deploy-manifests.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 kubectl create ns application
 kubectl create ns stress-test
-kubectl create ns stress-test-cpu
+kubectl create ns javaserver
 kubectl label node worker1 dedicated=test
 kubectl create ns distributed-db
 
