2ea1db05dff4a1397518f2654acc20efc7ee82d6
pablopez <pablo.lopezzaldivar@sysdig.com>
2024-02-20 11:43:19 +0100

    helm ops with @paul
    Signed-off-by: pablopez <pablo.lopezzaldivar@sysdig.com>

--- common/prepare-track/install_with_helm.sh
+++ common/prepare-track/install_with_helm.sh
@@ -15,14 +15,14 @@ ACCESS_KEY=$2
 HELM_REGION_ID=$3
 SECURE_API_TOKEN=$4
 COLLECTOR=$5
-# HELM_OPTS=""
+HELM_OPTS="${HELM_OPTS}"
 
 HELM_OPTS="--set agent.sysdig.settings.falcobaseline.report_interval=150000000000 \
 --set agent.sysdig.settings.falcobaseline.max_drops_buffer_rate_percentage=0.99 \
 --set agent.sysdig.settings.falcobaseline.max_sampling_ratio=128 \
 --set agent.sysdig.settings.falcobaseline.debug_metadata=true \
 --set agent.sysdig.settings.falcobaseline.debug=true \
---set agent.sysdig.settings.falcobaseline.randomize_start=false "
+--set agent.sysdig.settings.falcobaseline.randomize_start=false $HELM_OPTS"
 
 
 # new hostnames, to avoid duplicated names as much as possible
