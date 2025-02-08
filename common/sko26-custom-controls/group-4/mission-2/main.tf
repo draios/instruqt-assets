a5e1ff9feed804d05c7b27963f3cb03934088fc5
Daniel Céspedes <daniel.cespedes@sysdig.com>
2025-02-08 01:22:34 +0100

    Update modified files

--- common/sko26-custom-controls/group-4/mission-2/main.tf
+++ common/sko26-custom-controls/group-4/mission-2/main.tf
@@ -1,8 +1,8 @@
 ## Enter your Terraform code for your custom control
 
 resource "sysdig_secure_posture_control" "kubernetes_namespace_contain_argocd_hook_set_presync" {
-  name                = "Custom - Kubernetes Namespace - Namespace is configured to use ArgoCD Hook with PreSync mode - ${var.group_id}"
-  description         = "Custom - Kubernetes Namespace - Namespace is configured to use ArgoCD Hook with PreSync mode"
+  name                = "Custom - SKO26 - Kubernetes Namespace - Namespace is configured to use ArgoCD Hook with PreSync mode - ${var.group_id}"
+  description         = "Custom - SKO26 - Kubernetes Namespace - Namespace is configured to use ArgoCD Hook with PreSync mode"
   resource_kind       = "NAMESPACE"
   severity            = "High"
   rego                = # Enter the right REGO code to comply with your mandate
