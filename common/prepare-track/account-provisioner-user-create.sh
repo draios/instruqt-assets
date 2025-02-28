0738052ad55e6b755230de2ba2fb3fc146daeca7
Alvaro Iradier <airadier@gmail.com>
2025-02-28 08:31:24 +0100

    feat: Add parameter to user provisiing to enable/disable cloud onboarding

--- common/prepare-track/account-provisioner-user-create.sh
+++ common/prepare-track/account-provisioner-user-create.sh
@@ -13,11 +13,10 @@
 
 set -euxo pipefail
 
-if [ $# -ne 6 ]
-  then
-    echo "$0: Provide 6 arguments: "
+if [ $# -lt 6 ]; then
+    echo "$0: Provide at least 6 arguments."
     echo "$0: Defaulting to training account."
-    
+
     # parent account data, we create with pablo.lopezzaldivar+training@sysdig.com token
     ACCOUNT_PROVISIONER_MONITOR_API_TOKEN=[REDACTED]
     ACCOUNT_PROVISIONER_MONITOR_API_URL=https://us2.app.sysdig.com
@@ -32,7 +31,17 @@ else
     ACCOUNT_PROVISIONER_SECURE_API_URL=$4
     ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY=$5
     ACCOUNT_PROVISIONER_REGION_NUMBER=$6
+    ACCOUNT_PROVISIONER_DISABLE_ONBOARD=${7:-"0"}
 fi
+
+# Print values for debugging
+echo "ACCOUNT_PROVISIONER_MONITOR_API_TOKEN=$ACCOUNT_PROVISIONER_MONITOR_API_TOKEN"
+echo "ACCOUNT_PROVISIONER_MONITOR_API_URL=$ACCOUNT_PROVISIONER_MONITOR_API_URL"
+echo "ACCOUNT_PROVISIONER_SECURE_API_TOKEN=$ACCOUNT_PROVISIONER_SECURE_API_TOKEN"
+echo "ACCOUNT_PROVISIONER_SECURE_API_URL=$ACCOUNT_PROVISIONER_SECURE_API_URL"
+echo "ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY=$ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY"
+echo "ACCOUNT_PROVISIONER_REGION_NUMBER=$ACCOUNT_PROVISIONER_REGION_NUMBER"
+echo "ACCOUNT_PROVISIONER_DISABLE_ONBOARD=$ACCOUNT_PROVISIONER_DISABLE_ONBOARD"
 
 # TODO: get monitor operations team ID
 MONITOR_OPS_TEAM_ID=10018845
@@ -49,6 +58,7 @@ echo "${ACCOUNT_PROVISIONER_SECURE_API_TOKEN}" > $WORK_DIR/ACCOUNT_PROVISIONER_S
 echo "${ACCOUNT_PROVISIONER_SECURE_API_URL}" > $WORK_DIR/ACCOUNT_PROVISIONER_SECURE_API_URL
 echo "${ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY}" > $WORK_DIR/ACCOUNT_PROVISIONER_AGENT_ACCESS_KEY
 echo "${ACCOUNT_PROVISIONER_REGION_NUMBER}" > $WORK_DIR/ACCOUNT_PROVISIONER_REGION # check region ids in init.sh
+echo "${ACCOUNT_PROVISIONER_DISABLE_ONBOARD}" > $WORK_DIR/ACCOUNT_PROVISIONER_DISABLE_ONBOARD
 
 source $TRACK_DIR/lab_random_string_id.sh
 
@@ -84,31 +94,38 @@ curl -s -k -X POST \
 # set flag user provisioned, all OK
 touch $WORK_DIR/user_provisioned_COMPLETED
 
-# disable onboarding, just in case someone enables it
-# this is not working today, always return "onboardingEnabled":true (reported)
-# curl -s -k -X POST \
-# -H "Content-Type: application/json" \
-# -H "Authorization: Bearer ${ACCOUNT_PROVISIONER_SECURE_API_TOKEN}" \
-# --data-binary '{
-#     "onboardingEnabled":false,
-#     "newOnboardingWizard":false,
-#     "falcoCloudEnabled":false,
-#     "falcoCloudBetaFlows":false,
-#     "onboardingSkipped":false,
-#     "oktaEnabled":false}' \
-# ${ACCOUNT_PROVISIONER_SECURE_API_URL}/api/secure/onboarding/v2/feature/status \
-# | jq > /dev/null
-
 # get user id and API token
 SPA_USER_ID=$(cat  $WORK_DIR/account.json | jq .user.id)
 SPA_USER_API_TOKEN=$(cat  $WORK_DIR/account.json | jq -r  .token.key)
 
-# disable onboarding for this particular user
-curl -s -k -X POST \
--H "Content-Type: application/json" \
--H "Authorization: Bearer ${SPA_USER_API_TOKEN}" \
---data-binary @- \
-"${ACCOUNT_PROVISIONER_SECURE_API_URL}"/api/secure/onboarding/v2/userProfile/questionnaire <<EOF > /dev/null
+
+# Conditional block: Execute only if ACCOUNT_PROVISIONER_DISABLE_ONBOARD is "1"
+if [ "$ACCOUNT_PROVISIONER_DISABLE_ONBOARD" == "1" ]; then
+    echo "Onboarding is disabled. Skipping provisioning steps..."
+
+
+  # disable onboarding, just in case someone enables it
+  # this is not working today, always return "onboardingEnabled":true (reported)
+  # curl -s -k -X POST \
+  # -H "Content-Type: application/json" \
+  # -H "Authorization: Bearer ${ACCOUNT_PROVISIONER_SECURE_API_TOKEN}" \
+  # --data-binary '{
+  #     "onboardingEnabled":false,
+  #     "newOnboardingWizard":false,
+  #     "falcoCloudEnabled":false,
+  #     "falcoCloudBetaFlows":false,
+  #     "onboardingSkipped":false,
+  #     "oktaEnabled":false}' \
+  # ${ACCOUNT_PROVISIONER_SECURE_API_URL}/api/secure/onboarding/v2/feature/status \
+  # | jq > /dev/null
+
+
+  # disable onboarding for this particular user
+  curl -s -k -X POST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer ${SPA_USER_API_TOKEN}" \
+  --data-binary @- \
+  "${ACCOUNT_PROVISIONER_SECURE_API_URL}"/api/secure/onboarding/v2/userProfile/questionnaire <<EOF > /dev/null
 [
   {
     "id": "additionalEnvironments",
@@ -132,6 +149,12 @@ curl -s -k -X POST \
   }
 ]
 EOF
+
+else
+    echo "Onboarding is enabled."
+fi
+
+
 
 # get monitor operations team info
 curl -s -k -X GET \
